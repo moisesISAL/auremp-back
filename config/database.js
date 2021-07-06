d6342b0a1967953e3ae5e0cebe7c83aa65b935ca
moisesISAL <moises@isaldigital.com>
2021-07-06 18:22:36 -0500

    heroku deploy

--- config/database.js
+++ config/database.js
@@ -4,14 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: 'mongodb+srv://moises:Auremp2021@strapi.anh9r.mongodb.net/Strapi?retryWrites=true&w=majority',
-        srv: true,
-        port: 27017,
-        database: 'Strapi',
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
-        authenticationDatabase: null,
-        ssl: true
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
