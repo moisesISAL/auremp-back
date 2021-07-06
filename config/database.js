3d5990d2c2d9822c2f53d7aae3053e8c0afaef5a
moisesISAL <moises@isaldigital.com>
2021-07-05 22:10:05 -0500

    qo mongo

--- config/database.js
+++ config/database.js
@@ -2,14 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: 'mongodb+srv://moises:Auremp2021@strapi.anh9r.mongodb.net/Strapi?retryWrites=true&w=majority',
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: 'Strapi',
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
-});
+})
