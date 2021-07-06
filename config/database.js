c41bee2fdb90207c1469c08b1d68632b8e63ff22
moisesISAL <moises@isaldigital.com>
2021-07-05 22:12:56 -0500

    qo mongo 2

--- config/database.js
+++ config/database.js
@@ -5,13 +5,13 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         uri: 'mongodb+srv://moises:Auremp2021@strapi.anh9r.mongodb.net/Strapi?retryWrites=true&w=majority',
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
+        srv: true,
+        port: 27017,
         database: 'Strapi',
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: null,
+        ssl: true
       },
     },
   },
